48f5d56f5af1366789b5160432fb470d5fe8c24c
Văn Đức Nguyễn <nguyenvanduc19102000@gmail.com>
2022-05-24 22:26:16 +0700

    Layout Mac Dinh

---
+++ src/component/Layout/DefaultLayout/SideBar/index.js
@@ -0,0 +1,11 @@
+
+import classNames  from 'classnames/bind';
+import styles from './SideBar.module.scss'
+
+
+const cx = classNames.bind(styles)
+function SideBar() {
+    return ( <aside className={cx('wrapper')}>SideBar</aside> );
+}
+
+export default SideBar;
